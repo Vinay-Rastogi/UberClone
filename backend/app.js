e7c02817681c60cbcb8793b217940494e350ec1f
Vinay Rastogi <mvinayrastogi@gmail.com>
2024-12-23 22:31:41 +0530

    backend apis completed for rides

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const connectToDb = require("./db/db")
 const userRoutes = require("./routes/user.routes");
 const captainRoutes = require("./routes/captain.routes");
 const mapsRoutes = require("./routes/maps.routes")
+const rideRoutes = require("./routes/ride.routes");
 const cookieParser = require("cookie-parser");
 
 dotenv.config();
@@ -24,5 +25,6 @@ app.get('/',(req,res)=>{
 app.use('/users',userRoutes);
 app.use('/captains',captainRoutes);
 app.use('/maps',mapsRoutes);
+app.use('/rides',rideRoutes);
 
 module.exports = app;
